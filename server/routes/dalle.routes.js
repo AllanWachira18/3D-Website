3bc0f72eb3f8cdad08964ba24a114df2797b1ed6
AllanWachira18 <125892295+AllanWachira18@users.noreply.github.com>
2023-12-20 16:25:25 +0000

    fixed deployment 2

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -7,11 +7,13 @@ dotenv.config();
 
 const router = express.Router();
 
-const config = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+//const config = new Configuration({
+  //apiKey: process.env.OPENAI_API_KEY,
+//});
 
-const openai = new OpenAIApi(config);
+const openai = new OpenAIApi({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
 
 router.route('/').get((req, res) => {
   res.status(200).json({ message: "Hello from DALL.E ROUTES" })
